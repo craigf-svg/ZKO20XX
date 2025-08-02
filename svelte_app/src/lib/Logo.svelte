68cdb37fc3137bc18aa56fc0bb331feb103e5f4c
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-08-01 20:02:49 -0400

    Add logo SVG and toggle logo style

--- svelte_app/src/lib/Logo.svelte
+++ svelte_app/src/lib/Logo.svelte
@@ -1,15 +1,50 @@
+<script>
+    console.log("hello?");
+    let { show } = $props();
+    $effect(() => console.log("show logo", show));
+</script>
+
+{#if show}
+    <p id="zko" class="flex justify-between">
+        ZK
+        <span class="flex justify-between">
+            <img
+                src="/logo.svg"
+                alt="logo"
+                id="shine"
+                height="28px"
+                width="28px"
+            />
+            _2
+        </span>
+        0XX
+    </p>
+{:else}
+    <p id="zko" class="flex justify-between">
+        ZK<span>0_2</span>0XX
+        <img
+            src="/logo.svg"
+            alt="logo"
+            height="28px"
+            width="28px"
+            style="margin-left: -2.5px"
+        />
+    </p>
+{/if}
+
 <style>
-  #zko {
-    text-transform: uppercase;
-    font-size: 1.4em;
-		color: var(--color-orange-logo);
-		cursor: pointer;
-		text-decoration: none;
-  }
-  #zko span {
-    color: var(--color-orange-logo-secondary); 
-  }
+    #zko {
+        text-transform: uppercase;
+        font-size: 1.4em;
+        color: var(--color-orange-logo);
+        cursor: pointer;
+        text-decoration: none;
+    }
+    #zko span {
+        color: var(--color-orange-logo-secondary);
+    }
+    #shine {
+        margin-left: -4.5px;
+        margin-right: -3.5px;
+    }
 </style>
-
-<p id="zko">ZK<span>O_2</span>0XX</p>
-
--- svelte_app/src/lib/Navbar.svelte
+++ svelte_app/src/lib/Navbar.svelte
@@ -1,70 +1,88 @@
 <script lang="ts">
-  import Logo from "$lib/Logo.svelte";
+    import Logo from "$lib/Logo.svelte";
 
-  type Theme = "light" | "dark" | "catppuccin";
+    type Theme = "light" | "dark" | "catppuccin";
 
-  const { theme, cycleTheme, testSidecarPing } = $props<{
-    theme: Theme;
-    cycleTheme: () => void;
-    testSidecarPing: () => void;
-  }>();
+    const { theme, cycleTheme, testSidecarPing } = $props<{
+        theme: Theme;
+        cycleTheme: () => void;
+        testSidecarPing: () => void;
+    }>();
+
+    let show = $state(true);
+    $effect(() => console.log("show", show));
 </script>
 
 <nav>
-  <ul>
-    <li><a href="/" class="link"><Logo /></a></li>
-    <li>
-      <button type="button" class="link" onclick={testSidecarPing}>Test Sidecar Ping</button>
-    </li>
-    <li>
-      <button type="button" class="link" onclick={cycleTheme}
-        >{theme === "dark" ? "🌙 Dark Mode" : theme === "catppuccin" ? "☕ Catppuccin" : "☀️ Light Mode"}</button
-      >
-    </li>
-    <li>
-      <a href="/lab" class="link">Lab</a>
-    </li>
-    <li>
-      <a href="/about" class="link">About</a>
-    </li>
-    <li>
-      <a href="/settings" class="link">Settings</a>
-    </li>
-  </ul>
+    <ul>
+        <li>
+            <a href="/" class="link">
+                <Logo {show} />
+            </a>
+        </li>
+        <li>
+            <button type="button" class="link" onclick={() => (show = !show)}>
+                Switch
+            </button>
+        </li>
+        <li>
+            <button type="button" class="link" onclick={testSidecarPing}
+                >Test Sidecar Ping</button
+            >
+        </li>
+        <li>
+            <button type="button" class="link" onclick={cycleTheme}
+                >{theme === "dark"
+                    ? "🌙 Dark Mode"
+                    : theme === "catppuccin"
+                      ? "☕ Catppuccin"
+                      : "☀️ Light Mode"}</button
+            >
+        </li>
+        <li>
+            <a href="/lab" class="link">Lab</a>
+        </li>
+        <li>
+            <a href="/about" class="link">About</a>
+        </li>
+        <li>
+            <a href="/settings" class="link">Settings</a>
+        </li>
+    </ul>
 </nav>
 
 <style>
-  :root {
-    --navbar-height: 46px;
-  }
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: var(--color-bg-navbar);
-    display: flex;
-    height: var(--navbar-height);
-    align-items: center;
-  }
-  li {
-    padding: 0;
-  }
-  li:first-child {
-    margin-right: auto;
-  }
-  .link {
-    height: var(--navbar-height);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    color: var(--color-text-main);
-    font-weight: 100;
-    padding: 0 8px;
-  }
-  .link:hover {
-    background-color: var(--color-border);
-    cursor: pointer;
-  }
+    :root {
+        --navbar-height: 46px;
+    }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: var(--color-bg-navbar);
+        display: flex;
+        height: var(--navbar-height);
+        align-items: center;
+    }
+    li {
+        padding: 0;
+    }
+    li:first-child {
+        margin-right: auto;
+    }
+    .link {
+        height: var(--navbar-height);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        color: var(--color-text-main);
+        font-weight: 100;
+        padding: 0 8px;
+    }
+    .link:hover {
+        background-color: var(--color-border);
+        cursor: pointer;
+    }
 </style>
