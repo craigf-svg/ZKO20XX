6db70bbce65a04aef004b6cee81d408ac79c8799
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-11-13 23:56:35 -0500

    Remove dotenv

--- svelte_app/src-tauri/src/version.rs
+++ svelte_app/src-tauri/src/version.rs
@@ -1,7 +1,3 @@
-use dotenv::dotenv;
-use std::env;
-
 pub fn get_app_version() -> String {
-    dotenv().ok(); // Loads .env at runtime
-    env::var("APP_VERSION").unwrap_or_else(|_| env!("CARGO_PKG_VERSION").to_string())
+    std::env::var("APP_VERSION").unwrap_or_else(|_| env!("CARGO_PKG_VERSION").to_string())
 }
